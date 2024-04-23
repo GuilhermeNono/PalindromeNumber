2ff3df0fb69b35f8b0f746da62bd11c4e9541347
Guilherme Nono <guilherme.nono@uppercase.com.br>
2024-04-23 14:03:25 -0300

    Finishing Exercise

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PalindromeNumber;
+
+var number = Console.ReadLine();
+
+Console.WriteLine(Solution.IsPalindrome(Convert.ToInt32(number)));
---
+++ PalindromeNumber/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace PalindromeNumber;
+
+public static class Solution
+{
+    public static bool IsPalindrome(int x)
+    {
+        char[] xArray = x.ToString().ToCharArray();
+        char[] xReverseArray = xArray.Reverse().ToArray();
+
+        return (new string(xArray)).Equals(new string(xReverseArray));
+    } 
+}
